05859fca5ec1c7869476cdc88e37649039bf4899
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-11 12:21:17 +0900

    Exercise

---
+++ BasicJava/src/chapter02/Exercise21.java
@@ -0,0 +1,25 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Exercise21 {
+	public static void main(String[] args) {
+		double width;
+		double height;
+		double area;
+		double perimeter;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("가로의 길이는?(단위: m): ");
+		width = scanner.nextDouble();
+		System.out.print("세로의 길이는?(단위: m): ");
+		height = scanner.nextDouble();
+		
+		area = width * height;
+		perimeter = 2 * (width + height);
+		
+		System.out.println("직사각형의 넓이: " + area);
+		System.out.println("직사각형의 둘레: " + perimeter);
+	}
+}
---
+++ BasicJava/src/chapter02/Exercise24.java
@@ -0,0 +1,26 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Exercise24 {
+	public static void main(String[] args) {
+		Scanner scanner =  new Scanner(System.in);
+		double radius;
+		double height;
+		double area;
+		double volume;
+		double pi =  Math.PI;
+		
+		System.out.print("원기둥 밑변의 반지름을 입력하시오.(단위 : cm): ");
+		radius = scanner.nextDouble();
+		
+		System.out.print("원기둥의 높이를 입력하시오.(단위: cm): ");
+		height = scanner.nextDouble();
+		
+		area = radius * radius * pi;
+		volume = area * height;
+		
+		System.out.println("원기둥 밑변의 넓이는 " + area + "㎠이고," + "원기둥의 부피는 " + volume + "㎤이다.");
+		
+	}
+}
