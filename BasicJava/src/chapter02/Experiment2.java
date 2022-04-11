19ac24b6f2039d079a0809ca1a9c9a09b7a4fbe8
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-11 10:53:33 +0900

    second

--- BasicJava/src/chapter02/Experiment2.java
+++ BasicJava/src/chapter02/Experiment2.java
@@ -23,6 +23,7 @@ public class Experiment2 {
 		
 		String tel1 = "010"; String tel2 = "123"; String tel3 = "1234";
 		System.out.printf("전화: %1$s-%2$s-%3$s", tel1, tel2, tel3);
+		//1$ -> 첫번째 변수를 사용한다는 의미, 2$두번쨰, 3$세번째
 	}
 		
 	}
