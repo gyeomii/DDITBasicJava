55de2a0474bc81ee55889d840361f2b16b40a0dd
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-11 10:47:15 +0900

    commit

---
+++ BasicJava/src/chapter02/Experiment2.java
@@ -0,0 +1,25 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Experiment2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("[필수정보입력]");
+		System.out.print("1. 이름: ");
+		String name = scanner.nextLine();
+		System.out.print("2. 주민번호 앞 6자리: ");
+		String privatNum = scanner.nextLine();
+		System.out.print("3. 전화번호: ");
+		String telNum = scanner.nextLine();
+		
+		System.out.println("[입력한 내용]");
+		System.out.println("1.이름: " + name);
+		System.out.println("2. 주민번호 앞 6자리: " + privatNum);
+		System.out.println("3. 전화번호: " + telNum);
+	}
+		
+	}
--- BasicJava/src/chapter02/ScannerExample.java
+++ BasicJava/src/chapter02/ScannerExample.java
@@ -12,7 +12,11 @@ public class ScannerExample {
 	  scanner.nextLine();//문자 입력받기(엔터가 입력된 부분까지)
 	  scanner.nextInt();//정수 입력받기
 	  scanner.nextDouble();//실수 입력받기
-	  */
+	  
+	   * printf("잔액: %,d, 1000000);
+	   * printf("소숫점 둘째자리: %.2f, 10.21355);
+	   * 혼공자 책 91p 참조하기
+	   */
 	  while(true) {
 	  System.out.print("이름을 입력하세요: ");
 	  String stringData = scanner.next();
@@ -24,7 +28,6 @@ public class ScannerExample {
 	  System.out.print("나이를 입력하세요: ");
 	  int intData = scanner.nextInt();
 
-	  
 	  	System.out.println("이름: " + stringData + "\n나이: " + intData);
 	  	System.out.printf("이름: %s\n나이: %d\n",stringData, intData);
 	  }
