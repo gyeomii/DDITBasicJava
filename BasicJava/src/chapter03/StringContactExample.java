b154e72e6e72273ffd435932f60bfbb8aa9f2420
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-12 11:59:37 +0900

    example

--- BasicJava/src/chapter03/StringContactExample.java
+++ BasicJava/src/chapter03/StringContactExample.java
@@ -5,6 +5,7 @@ public class StringContactExample {
 		String str1 = "JDK" + 6.0;
 		String str2 = str1 + "특징";
 		System.out.println(str2);
+		
 		String str3 = "JDK" + 3 + 3.0;
 		String str4 = 3 + 3.0 + "JDK";
 		System.out.println(str3);
