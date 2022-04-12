d3f9e26d272ca2b1040bf8cd9998f3b4fda76ca0
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-12 12:34:19 +0900

    comment

--- BasicJava/src/chapter03/CompareOperatorExample2.java
+++ BasicJava/src/chapter03/CompareOperatorExample2.java
@@ -9,6 +9,8 @@ public class CompareOperatorExample2 {
 		double v4 = 0.1;
 		float v5 = 0.1f;
 		System.out.println(v4 == v5);
+		//double타입에 0.1을 입력하더라도 정확한 0.1이 아닌 double범위내의 근사값이 들어있고
+		//float타입도 근사치가 들어있기 때문에 두 값을 비교하면 false가 나온다.
 		System.out.println((float)v4 == v5);
 	}
 }
