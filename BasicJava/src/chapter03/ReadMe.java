e7fcad8fdee8c7ea32eba01e838d6eda32d16dff
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-12 11:43:47 +0900

    Example

--- BasicJava/src/chapter03/ReadMe.java
+++ BasicJava/src/chapter03/ReadMe.java
@@ -1,5 +1,26 @@
 package chapter03;
 
 public class ReadMe {
-	//This is ReadME file for commit new package to git
+	public static void main(String[] args) {
+		
+	//Summary 22.04.12
+	/*
+	 * 프로그래밍 : 데이터 [ 변수, 연산자, 제어문, 메소드]
+	 * 	변수 : 선언, 초기화										 |
+	 * 	연산자 : 산술, 부호, 문자열, 대입, 증감, 비교, 논리, 조건|
+	 * 	제어문 : 반복문, 조건문									 | -> 객체 -> 추상 -> 인터페이스
+	 * 	메소드													 |
+	 * 
+	 *  - 자바 연산자 : 기본타입만 연산 가능
+	 *  
+	 *  대입연산자의 우선순위가 연산자중에 가장 낮다.
+	 */
+	int a = 2020;
+	int b = 5;
+	double c = 15 % 3.4176;
+	
+	System.out.printf("%d, %d\n", ++b, b );
+	System.out.printf("%f, %f", c++, c );
+	
 	}
+}
