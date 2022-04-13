215e806cb8d7c7bc5359764d41fdd9dd31668c4d
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-13 17:15:40 +0900

    Example

--- BasicJava/src/chapter03/Exercise32.java
+++ BasicJava/src/chapter03/Exercise32.java
@@ -13,7 +13,7 @@ public class Exercise32 {
 		System.out.print("연 소득이 1억 이상인 인구는?(단위: 만)");
 		int income = scanner.nextInt();
 		//data processing
-		String metro = ((capital == 1) && (popularity >= 100)) ? "메트로폴리스입니다." : ( (income >= 50) ? "메트로폴리스입니다." : "메트로폴리스가 아닙니다.");
+		String metro = ((capital == 1) && (popularity >= 100)) || (income >= 50)  ? "메트로폴리스입니다." : "메트로폴리스가 아닙니다.";
 		//output
 		System.out.println("이 도시는 " + metro);
 	}
--- BasicJava/src/chapter03/Exercise33.java
+++ BasicJava/src/chapter03/Exercise33.java
@@ -10,7 +10,7 @@ public class Exercise33 {
 		System.out.print("윤년인지를 확인할 연도를 입력하세요: ");
 		int year = scanner.nextInt();
 		//data processing
-		String leapYear = ((year % 4 == 0) && (year % 100 != 0)) ? "윤년 입니다." : ((year % 400 == 0) ? "윤년 입니다." : "윤년이 아닙니다.");
+		String leapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? "윤년 입니다." : "윤년이 아닙니다.";
 		//output
 		System.out.println(year + "년은 " + leapYear);
 	}
