f906ec4079903853470e142ee9d4027a5893c466
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-14 09:19:56 +0900

    Example

--- BasicJava/src/chapter03/Example9.java
+++ BasicJava/src/chapter03/Example9.java
@@ -16,13 +16,15 @@ public class Example9 {
 	
 	    double str1Double = Double.parseDouble(str1);
 	    double str2Double = Double.parseDouble(str2);
+	    double result = str1Double / str2Double;
 	System.out.println("-----------------");
-	    if(str2Double != 0.0) {
-	    	double result = str1Double / str2Double;
+	
+	 if(str2Double != 0.0) {
     	System.out.println("결과:" + result);
 	    }
-	    else
+	    else 
 	    	System.out.println("결과:무한대");
 		}
 }
+
 
