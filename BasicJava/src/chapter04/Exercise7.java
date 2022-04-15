e325a2c2d04b18986c576171ab0de95c02770399
PC 06 <PC 06@DESKTOP-3J1D651>
2022-04-15 12:16:23 +0900

    Exercise

---
+++ BasicJava/src/chapter04/Exercise7.java
@@ -0,0 +1,44 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+	public static void main(String[] args) {
+		boolean run = true;
+		int balance = 0;
+		int credit = 0;
+		int outMoney = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("----------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("----------------------------------");
+			System.out.print("선택> ");
+			int getNum = scanner.nextInt();
+				while(getNum == 1) {
+					System.out.print("예금액> ");
+					credit = scanner.nextInt();
+					balance = credit;
+					break;
+				}
+				while(getNum == 2) {
+					System.out.print("출금액> ");
+					outMoney = scanner.nextInt();
+					balance -= outMoney;
+					break;
+				}
+				while(getNum == 3) {
+					System.out.println("잔고> " + balance);
+					break;
+				}
+				while(getNum == 4){
+					run = false;
+					break;
+				}
+			}
+		System.out.println("             ");
+		System.out.println("프로그램 종료");
+		}
+}
+
