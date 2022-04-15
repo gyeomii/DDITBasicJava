9f5ad19206308a874e963073f574aa636a00b94e
gyeomii <kimsg7388@gmail.com>
2022-04-15 14:57:30 +0900

    Exercise

--- BasicJava/src/chapter04/Exercise5.java
+++ BasicJava/src/chapter04/Exercise5.java
@@ -4,7 +4,7 @@ public class Exercise5 {
 	public static void main(String[] args) {
 		for(int i = 1; i < 5; i++) {
 			for(int j = 1; j < 5; j++) {
-				if(i>=j) {
+				if(i >= j) {
 				System.out.print("*");
 				}
 				else {
--- BasicJava/src/chapter04/Exercise6.java
+++ BasicJava/src/chapter04/Exercise6.java
@@ -4,7 +4,7 @@ public class Exercise6 {
 	public static void main(String[] args) {
 		for(int i = 4; i > 0; i--) {
 			for(int j = 1; j < 5; j++) {
-				if(i>j) {
+				if(i > j) {
 				System.out.print(" ");
 				}
 				else {
