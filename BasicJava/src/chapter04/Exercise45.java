249ecc9616cc8609f03f2e150a9ff75ad14f2d51
gyeomii <kimsg7388@gmail.com>
2022-04-19 12:37:48 +0900

    RandomNumberExercise

--- BasicJava/src/chapter04/Exercise45.java
+++ BasicJava/src/chapter04/Exercise45.java
@@ -10,20 +10,20 @@ public class Exercise45 {
 		int randomNum = random.nextInt(100)+1;
 		int inputNum;
 		int count = 0;
-		
+	
 		 do {
 	          System.out.print("1부터 100 사이의 정수 중 하나를 선택하세요: ");
 	          inputNum = scanner.nextInt();
 
-	          if(randomNum>inputNum){
+	          if(randomNum > inputNum){
 	        	  System.out.println("정답은 더 큰 수 입니다.");
-	        	  }else if(randomNum<inputNum){
+	        	  }else if(randomNum < inputNum){
 	        	  System.out.println("정답은 더 작은 수 입니다.");
 	        	  }
 	          count++;
 	        }               
-	        while (randomNum!=inputNum);
-	        System.out.println("정답입니다. " + count + "번 만에 맞추셨습니다\n 게임을 종료합니다.") ;
+	        while (randomNum != inputNum);
+	        System.out.println("정답입니다. " + count + "번 만에 맞추셨습니다\n게임을 종료합니다.") ;
 	        scanner.close();
 	}
 }
