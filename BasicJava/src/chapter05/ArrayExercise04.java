a6d0248b4a8f3b651fe4b6633e8d44b6906d72e5
gyeomii <kimsg7388@gmail.com>
2022-04-22 10:18:08 +0900

    Array04

--- BasicJava/src/chapter05/ArrayExercise04.java
+++ BasicJava/src/chapter05/ArrayExercise04.java
@@ -4,14 +4,24 @@ import java.util.Iterator;
 
 public class ArrayExercise04 {
 	public static void main(String[] args) {
-		int max = 0;
+		
+		  //int max = Integer.MIN_VALUE;
+	   	  //int min = Integer.MAX_VALUE;
+		
 		int array[] = { 1, 5, 3, 8, 2 };
+
+		int max = array[0];
+		int min = array[0];
 		
 		for (int i = 0; i < array.length; i++) {
-			if(max < array[i])
+			if(max < array[i]) {
 				max = array[i];
+			}else if(min > array[i]) {
+				min = array[i];
+			}
 		}
 		
 		System.out.println("max: " + max);
+		System.out.println("min: " + min);
 	}
 }
