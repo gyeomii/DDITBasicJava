0f0546cbb1187eb6f86beec133ba2daa41308d59
gyeomii <kimsg7388@gmail.com>
2022-04-25 09:16:10 +0900

    edit

--- BasicJava/src/chapter05/Exercise51.java
+++ BasicJava/src/chapter05/Exercise51.java
@@ -3,8 +3,10 @@ package chapter05;
 public class Exercise51 {
 	public static void main(String[] args) {
 		int[] array = {38, 94, 16, 3, 76, 94, 82, 47, 59, 8};
-		int max = array[0];
-		int min = array[0];
+		//int max = array[0];//0번째 인덱스가 없으면 오류
+		//int min = array[0];
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 		
 		for (int i = 0; i < array.length; i++) {
 			if(max < array[i]) { //최대
