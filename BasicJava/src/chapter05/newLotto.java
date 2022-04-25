99c459a4fa3e46e785f62afd224d99507691f5f5
gyeomii <kimsg7388@gmail.com>
2022-04-25 12:19:59 +0900

    newLotto

--- BasicJava/src/chapter05/newLotto.java
+++ BasicJava/src/chapter05/newLotto.java
@@ -2,17 +2,17 @@ package chapter05;
 
 import java.util.Random;
 
-public class NewLotto {
+public class newLotto {
 	public static void main(String[] args) {
 		Random random = new Random();
 		
 		int[] lottoNum = new int[45];
 		
-		for (int i = 0; i < lottoNum.length; i++) {
+		for (int i = 0; i < lottoNum.length; i++) { //배열에 1 ~ 45저장
 			lottoNum[i] = i+1;
 		}
 		
-	     for (int i = lottoNum.length - 1; i >= 0; i--) {
+	     for (int i = lottoNum.length - 1; i >= 0; i--) { //배열값 섞기
              
 	            int j = random.nextInt(i+1);
 	             
@@ -20,9 +20,10 @@ public class NewLotto {
 	            lottoNum[i] = lottoNum[j];
 	            lottoNum[j] = temp;
 	     }	
+	     
+	     System.out.print("로또번호: ");
 	     for (int j = 0; j < 6; j++) {
 	    	 System.out.print(lottoNum[j] + " ");
 		}
-	     
 	}
 }
