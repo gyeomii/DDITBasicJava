61de04368661417e26e866d89011857ddd145f21
gyeomii <kimsg7388@gmail.com>
2022-04-26 11:41:53 +0900

    Member class

--- BasicJava/src/chapter06/Member.java
+++ BasicJava/src/chapter06/Member.java
@@ -5,4 +5,12 @@ public class Member {
 	String id;
 	String password;
 	int age;
+	
+	Member(String name, String id, String password, int age){
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.age = age;
+		
+	}
 }
--- BasicJava/src/chapter06/MemberExample.java
+++ BasicJava/src/chapter06/MemberExample.java
@@ -2,19 +2,9 @@ package chapter06;
 
 public class MemberExample {
 	public static void main(String[] args) {
-		Member member1 = new Member();
-		Member member2 = new Member();
-		
-		member1.name = "김성겸";
-		member1.id = "kimsg";
-		member1.password = "0001";
-		member1.age = 26;
-		
-		member2.name = "겸성김";
-		member2.id = "gyeomsk";
-		member2.password = "0002";
-		member2.age = 62;
-		
+		Member member1 = new Member("김성겸", "kimsg", "0001", 26);
+		Member member2 = new Member("겸성김", "gyeomsk", "0002", 62);
+
 		System.out.println("이름: " + member1.name);
 		System.out.println("아이디: " + member1.id);
 		System.out.println("패스워드: " + member1.password);
