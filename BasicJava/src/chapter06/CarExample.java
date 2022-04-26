32686f22253c0a58bd6dd8feeef5ae4e362544eb
gyeomii <kimsg7388@gmail.com>
2022-04-26 11:12:11 +0900

    Car class

--- BasicJava/src/chapter06/CarExample.java
+++ BasicJava/src/chapter06/CarExample.java
@@ -3,17 +3,25 @@ package chapter06;
 public class CarExample {
 	public static void main(String[] args) {
 		Car car1 = new Car();
+		System.out.println("car1.company : "+ car1.company);
+		System.out.println();
 		
-		System.out.println("제작회사: " + car1.company);
-		System.out.println("모델명: " + car1.model);
-		System.out.println("색: " + car1.color);
-		System.out.println("최고속도: " + car1.maxSpeed);
-		System.out.println("엔진상태: " + car1.engineStart);
-		System.out.println("현재속도: " + car1.speed);
+		Car car2 = new Car("자가용");
+		System.out.println("car2.company : " + car2.company);
+		System.out.println("car2.model : " + car2.model);
+		System.out.println();
 		
-		car1.engineStart = true;
-		car1.speed = 60;
-		System.out.println("수정된 엔진상태: " + car1.engineStart);
-		System.out.println("수정된 속도: " + car1.speed);
+		Car car3 = new Car("자가용", "빨강");
+		System.out.println("car3.company : " + car3.company);
+		System.out.println("car3.model : " + car3.model);
+		System.out.println("car3.color : " + car3.color);
+		System.out.println();
+		
+		Car car4 = new Car("자가용", "검정", 200);
+		System.out.println("car4.company : " + car4.company);
+		System.out.println("car4.model : " + car4.model);
+		System.out.println("car4.color : " + car4.color);
+		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+		System.out.println();
 	}
 }
