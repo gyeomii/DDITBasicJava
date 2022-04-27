654157ad936b9046c980aede8bfe901c3d7d1240
gyeomii <kimsg7388@gmail.com>
2022-04-27 12:09:39 +0900

    Car Method2

--- BasicJava/src/chapter06/Car.java
+++ BasicJava/src/chapter06/Car.java
@@ -3,9 +3,18 @@ package chapter06;
 public class Car {
 	
 	int gas;
+	int speed;
 	
 	void setGas(int gas) { //setGas메소드 생성
 		this.gas = gas; //받아온 데이터를 gas에 저장
+	}
+	
+	int getSpeed() { //getSpeed메소드 생성
+		return speed;
+	}
+	
+	void keyTurnOn() { //keyTurnOn메소드 생성
+		System.out.println("키를 돌립니다.");
 	}
 	
 	boolean isLeftGas() { //isLeftGas메소드 생성
@@ -21,10 +30,14 @@ public class Car {
 	void run() { //run메소드 생성
 		while(true) { //while 안에 true 이므로 항상 실행
 			if(gas > 0) { //gas가 0보다 크면 if문 실행
-				System.out.println("달립니다.(gas잔량: " + gas + ")");
-				gas -= 1;
+				for(int i = 10; i <= gas*10000; i+= 10) {
+					speed = i;
+					System.out.println("달립니다.(시속: " + speed + "km/h, " + "gas잔량: " + gas + ")");
+					gas -= 1;
+				}
 			}else { //gas가 0이면 else 실행
-				System.out.println("멈춥니다.(gas잔량: " + gas + ")");
+				speed = 0;
+				System.out.println("멈춥니다.(시속: " + speed + "km/h, " + "gas잔량: " + gas + ")");
 				return; //반환값이 없는 void타입이므로 메소드 종료
 			}
 		}
--- BasicJava/src/chapter06/CarExample.java
+++ BasicJava/src/chapter06/CarExample.java
@@ -2,17 +2,17 @@ package chapter06;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car mycar = new Car(); //Car()객체 인스턴스화
+		Car myCar = new Car(); //Car()객체 인스턴스화
+		myCar.keyTurnOn();
+		myCar.setGas(15); //setGas메소드 호출
 		
-		mycar.setGas(5); //setGas메소드 호출
-		
-		boolean gasState = mycar.isLeftGas(); //isLeftGas메소드 호출
+		boolean gasState = myCar.isLeftGas(); //gasState에 isLeftGas에서 호출된 값 저장
 		if(gasState) { //gasState가 true면 if문 실행
 			System.out.println("출발합니다.");
-			mycar.run(); //run()메소드 호출
+			myCar.run(); //run()메소드 호출
 		}
 		
-		if(mycar.isLeftGas()) { //isLeftGas()에서 리턴된 값이 true면 if문 실행
+		if(myCar.isLeftGas()) { //isLeftGas()에서 리턴된 값이 true면 if문 실행
 								//false면 else로 넘어감
 			System.out.println("gas를 주입할 필요가 없습니다.");
 		}else {
