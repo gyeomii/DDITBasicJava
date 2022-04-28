4b7bcb4f45bd88e76474526119a546e47bc54823
gyeomii <kimsg7388@gmail.com>
2022-04-28 10:07:30 +0900

    BaekJoon1110

---
+++ BasicJava/src/BaekJoon/PlusCycle.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class PlusCycle{
+	public static void main(String[] args) {
+		      Scanner scanner = new Scanner(System.in);
+		      System.out.print("숫자 입력: ");
+				int num = scanner.nextInt();
+				scanner.close();
+				
+				int count = 0;
+				int numCopy = num;
+		     
+				while (true) {
+					num = ((num % 10) * 10) + (((num / 10) + (num % 10)) % 10);
+					count++;
+		 
+					if (numCopy == num) {
+						break;
+					}
+				}
+				System.out.println(numCopy + "의 사이클 길이: " + count);
+				
+		      }
+}
+      
+
