c46486c50deb80ad6b342c6ba4a7215e595ee3e0
gyeomii <kimsg7388@gmail.com>
2022-04-28 12:39:21 +0900

    접근제한자

--- BasicJava/src/chapter06/package1/B.java
+++ BasicJava/src/chapter06/package1/B.java
@@ -4,16 +4,16 @@ public class B {
 	public B() {
 		A a1 = new A(true);
 		A a2 = new A(1);
-		A a3 = new A("문자열"); //private생성자 접근 불가
+		//A a3 = new A("문자열"); //private생성자 접근 불가
 	
 		A a = new A();
 		a.field1 = 1;
 		a.field2 = 1;
-		a.field3 = 1;//private필드 접근 불가
+		//a.field3 = 1;//private필드 접근 불가
 
 		a.method1();
 		a.method2();
-		a.method3();//private메소드 접근 불가
+		//a.method3();//private메소드 접근 불가
 	}
 	
 }
