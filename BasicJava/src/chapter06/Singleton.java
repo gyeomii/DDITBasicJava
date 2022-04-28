872f92c1ef0dfe22f6206a9e506560074cc66dec
gyeomii <kimsg7388@gmail.com>
2022-04-28 12:46:44 +0900

    Singleton Example

--- BasicJava/src/chapter06/Singleton.java
+++ BasicJava/src/chapter06/Singleton.java
@@ -2,8 +2,18 @@ package chapter06;
 
 public class Singleton {
 	private static Singleton singleton = new Singleton();
+	private int age = 0;
+	private String name = " ";
 	
 	private Singleton() {}
+	
+	static String getName(String name) {
+		return name;
+	}
+	
+	static int getAge(int age) {
+		return age;
+	}
 	
 	static Singleton getInstance() {
 		return singleton;
--- BasicJava/src/chapter06/SingletonExample.java
+++ BasicJava/src/chapter06/SingletonExample.java
@@ -5,6 +5,12 @@ public class SingletonExample {
 		Singleton obj1 = Singleton.getInstance();
 		Singleton obj2 = Singleton.getInstance();
 		
+		int obj1Age = obj1.getAge(26);
+		String obj1Name = obj1.getName("김성겸");
+		
+		System.out.println(obj1Name);
+		System.out.println(obj1Age);
+		
 		if(obj1 == obj2) {
 			System.out.println("같은 Singleton 객체입니다.");
 		}else {
