499119f2e2af00a79761f1993b337bae3fc1ec27
gyeomii <kimsg7388@gmail.com>
2022-04-29 12:34:04 +0900

    Car Example

--- BasicJava/src/chapter06/Car.java
+++ BasicJava/src/chapter06/Car.java
@@ -2,51 +2,29 @@ package chapter06;
 
 public class Car {
 	
-	int gas;
-	int speed;
+	//외부에서 객체의 필드를 직접 접근하는것을 막기위해 private로 선언
+	private int speed;
+	private boolean stop;
 	
-	void setGas(int gas) { //setGas메소드 생성
-		this.gas = gas; //받아온 데이터를 gas에 저장
-	}
-	
-	int getSpeed() { //getSpeed메소드 생성
+	//메소드를 통해서 필드를 변경
+	public int getSpeed() {
 		return speed;
 	}
 	
-	void keyTurnOn() { //keyTurnOn메소드 생성
-		System.out.println("키를 돌립니다.");
-	}
-	
-	boolean isLeftGas() { //isLeftGas메소드 생성
-		if(gas == 0) { //gas가 0이면 if문 실행
-			System.out.println("gas가 없습니다.");
-			return false; //false를 반환
-		}
-		System.out.println("gas가 있습니다.");
-		return true; //gas가 0이 아니면 true를 반환
-		
-	}
-	
-	void run() { //run메소드 생성
-		while(true) { //while 안에 true 이므로 항상 실행
-			if(gas > 0) { //gas가 0보다 크면 if문 실행
-				for(int i = 10; i <= gas*10000; i+= 10) {
-					speed = i;
-					System.out.println("달립니다.(시속: " + speed + "km/h, " + "gas잔량: " + gas + ")");
-					gas -= 1;
-				}
-			}else { //gas가 0이면 else 실행
-				speed = 0;
-				System.out.println("멈춥니다.(시속: " + speed + "km/h, " + "gas잔량: " + gas + ")");
-				return; //반환값이 없는 void타입이므로 메소드 종료
-			}
+	public void setSpeed(int speed) {
+		if(speed < 0) {
+			this.speed = 0;
+			return;
+		}else {
+			this.speed = speed;
 		}
 	}
 	
-	public static void main(String[] args) {
-		Car myCar = new Car();
-		myCar.speed = 60;
-		myCar.gas = 5;
-		myCar.run();
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		this.speed = 0;
 	}
 }
--- BasicJava/src/chapter06/CarExample.java
+++ BasicJava/src/chapter06/CarExample.java
@@ -2,21 +2,20 @@ package chapter06;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car myCar = new Car(); //Car()객체 인스턴스화
-		myCar.keyTurnOn();
-		myCar.setGas(15); //setGas메소드 호출
+		Car myCar = new Car();
+		//잘못된 속도 변경
+		myCar.setSpeed(-50);
 		
-		boolean gasState = myCar.isLeftGas(); //gasState에 isLeftGas에서 호출된 값 저장
-		if(gasState) { //gasState가 true면 if문 실행
-			System.out.println("출발합니다.");
-			myCar.run(); //run()메소드 호출
+		System.out.println("현재 속도: " + myCar.getSpeed());
+		
+		//올바른 속도 변경
+		myCar.setSpeed(60);
+		
+		//멈춤
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
 		}
 		
-		if(myCar.isLeftGas()) { //isLeftGas()에서 리턴된 값이 true면 if문 실행
-								//false면 else로 넘어감
-			System.out.println("gas를 주입할 필요가 없습니다.");
-		}else {
-			System.out.println("gas를 주입하세요.");
-		}
+		System.out.println("현재 속도: " + myCar.getSpeed());
 	}
 }
