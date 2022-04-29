202cbc7e302c1bf1f75ccffa14bd7fb4689c8152
gyeomii <kimsg7388@gmail.com>
2022-04-29 19:35:03 +0900

    .

--- BasicJava/src/chaptor06Example/Circle.java
+++ BasicJava/src/chaptor06Example/Circle.java
@@ -23,24 +23,20 @@ public class Circle {
 		return y;
 	}
 	
-	public double setRadius(double radius) {
+	public void setRadius(double radius) {
 		if(radius <0) { //만약 잘못된 값이 입력되면 반지름을 0으로 한다.
 			this.radius = 0;
-			return 0 ;
 		}else {
 			this.radius = radius;
 		}
-		return radius;
 	}
 	
-	public double setX(double x) {
+	public void setX(double x) {
 		this.x = x;
-		return x;
 	}
 	
-	public double setY(double y) {
+	public void setY(double y) {
 		this.y = y;
-		return y;
 	}
 	
 
