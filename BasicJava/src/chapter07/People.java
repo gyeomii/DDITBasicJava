874dcc7aee3ba76fad330b36ab1e3e6b8fb236fa
gyeomii <kimsg7388@gmail.com>
2022-05-02 11:48:28 +0900

    inheritance

--- BasicJava/src/chapter07/People.java
+++ BasicJava/src/chapter07/People.java
@@ -1,9 +1,11 @@
 package chapter07;
 
 public class People {
+	// 필드
 	public String name;
 	public String ssn;
 
+	// 생성자
 	public People(String name, String ssn) {
 		this.name = name;
 		this.ssn = ssn;
--- BasicJava/src/chapter07/Student.java
+++ BasicJava/src/chapter07/Student.java
@@ -1,8 +1,10 @@
 package chapter07;
 
 public class Student extends People {
+	// 필드
 	public int studentNo;
 
+	// 생성자
 	public Student(String name, String ssn, int studentNo) {
 		super(name, ssn);
 		this.studentNo = studentNo;
