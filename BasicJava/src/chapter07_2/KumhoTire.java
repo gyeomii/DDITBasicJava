a038fbc8e3e0a8a043f6f0bddccd25f694e404f9
gyeomii <kimsg7388@gmail.com>
2022-05-03 12:40:26 +0900

    Tire class  polymorphism Example

---
+++ BasicJava/src/chapter07_2/KumhoTire.java
@@ -0,0 +1,22 @@
+package chapter07_2;
+
+public class KumhoTire extends Tire {
+	//필드
+	//생성자
+	public KumhoTire (String location, int maxRotation) {
+		super(location, maxRotation);
+	}
+	
+	//메소드
+	@Override
+	public boolean roll() {
+		++accumulateRotation;
+		if(accumulateRotation < maxRotation) {
+			System.out.println(location + " KumhoTire 수명: " + (maxRotation - accumulateRotation) + "회");
+			return true;
+		}else {
+			System.out.println("***" + location + " KumhoTire 평크 ***");
+			return false;
+		}
+	}
+}
