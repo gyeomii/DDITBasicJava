fd83fea6b5b0e47b5ba5c8560a441c818b6ec4a1
gyeomii <kimsg7388@gmail.com>
2022-05-05 13:49:59 +0900

    Chapter07. inheritance Shape Example

---
+++ BasicJava/src/chapter07Example/Shape.java
@@ -0,0 +1,13 @@
+package chapter07Example;
+
+public class Shape {
+	// 메소드
+	public double area() {
+		return 0;
+	}
+
+	public double perimeter() {
+		return 0;
+	}
+
+}
---
+++ BasicJava/src/chapter07Example/Triangle.java
@@ -0,0 +1,26 @@
+package chapter07Example;
+
+public class Triangle extends Shape {
+	// 필드
+	private double side;
+
+	// 생성자
+	public Triangle(double side) {
+		this.side = side;
+	}
+
+	// 메소드
+	@Override
+	public double area() {
+		return (Math.sqrt(3) / 4) * side * side;
+	}
+
+	@Override
+	public double perimeter() {
+		return 3 * side;
+	}
+
+	public String toString() {
+		return String.format("도형의 종류: 삼각형, 둘레: %.1fcm, 넓이: %.16f㎠", perimeter(), area());
+	}
+}
