e906c9c04ef66583461bf338c6e94c7ea62ed2e0
gyeomii <kimsg7388@gmail.com>
2022-05-06 10:28:57 +0900

    수정

--- BasicJava/src/chapter07Example/Bank/BankAccount.java
+++ BasicJava/src/chapter07Example/Bank/BankAccount.java
@@ -18,21 +18,13 @@ public class BankAccount {
 	}
 
 	public boolean withdraw(int amount) { // 출금 메소드
-		if (balance < amount) { // 잔고 < 출금액 일 때
-			return false;
-		} else {
-			balance -= amount; // 잔고에서 출금액 만큼 출금
-			return true;
-		}
+		balance -= amount; // 잔고에서 출금액 만큼 출금
+		return true;
 	}
 
 	public boolean transfer(int amount, BankAccount otherAccount) { // 현재계좌에서 amount만큼 다른계좌로 송금하는 메소드
-		if (balance < amount) { // 잔고 < 출금액 일 때
-			return false;
-		} else {
-			balance -= amount; // 본인 잔고에서 출금액 만큼 출금
-			otherAccount.balance += amount; // 타인 잔고에 출금액을 입금
-			return true;
-		}
+		withdraw(amount);
+		otherAccount.deposit(amount);
+		return true;
 	}
 }
