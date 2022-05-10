d3a51a0c9fdf8063b721f21f99663abe28aac794
gyeomii <kimsg7388@gmail.com>
2022-05-10 09:50:06 +0900

    how to call method

---
+++ BasicJava/src/midtermExam/MyUtils.java
@@ -0,0 +1,13 @@
+package midtermExam;
+
+import java.util.List;
+
+public class MyUtils {
+	public static double avg(List<Integer> scores) {
+		return scores.stream().mapToInt(Integer::intValue).average().getAsDouble();
+	}
+
+	public long count(List<Integer> scores) {
+		return scores.stream().mapToInt(Integer::intValue).count();
+	}
+}
