432ec20496da0111b84eca54aeb18b0a9f7c7685
gyeomii <kimsg7388@gmail.com>
2022-05-11 15:20:24 +0900

    CHAPTER8 EXAMPLE

---
+++ BasicJava/src/chapter08Example/bank08/SavingsAccount.java
@@ -0,0 +1,22 @@
+package chapter08Example.bank08;
+
+public class SavingsAccount extends BankAccount {
+	// 필드
+	private double interestRate; // 이자율
+
+	// 생성자
+	public SavingsAccount(int balance, double interestRate) {
+		super(balance); // 초기화
+		this.interestRate = interestRate;
+	}
+
+	// 메소드
+	public void updateBalance(int period) {// 이자를 계산하여 잔액에 추가
+		balance += balance * interestRate * period;
+	}
+
+	@Override
+	public String getAccountType() {
+		return "저축예금";
+	}
+}
