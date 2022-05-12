7af0a15202093c901879b22b4889a8db1b416eca
gyeomii <kimsg7388@gmail.com>
2022-05-12 09:38:23 +0900

    edit

--- BasicJava/src/chapter10Example/exercise11/Exercise11.java
+++ BasicJava/src/chapter10Example/exercise11/Exercise11.java
@@ -7,28 +7,24 @@ public class Exercise11 {
 	public static void main(String[] args) {
 		boolean run = true;
 		Scanner scanner = new Scanner(System.in);
-		
-		while(run) {
+
+		while (run) {
 			try {
-			System.out.print("어떤 수를 나누시겠습니까?>> ");
-			int num = scanner.nextInt();
-			System.out.print("어떤 수로 나누시겠습니까?>> ");
-			int divider = scanner.nextInt();
-			// ArithmeticException이 있는줄 모르고 if문으로 RuntimeException을 던짐
-			/*if (num == 0 || divider == 0) {
-				throw new RuntimeException("0으로 나눌 수 없습니다. 다시 입력해주세요.");
-			} else {*/
-				int divideresult = num / divider;
-				System.out.println(num + "/" + divider + " = " + divideresult);
+				System.out.print("어떤 수를 나누시겠습니까?>> ");
+				double dividend = scanner.nextDouble();
+				System.out.print("어떤 수로 나누시겠습니까?>> ");
+				double divisor = scanner.nextDouble();
+
+				double quotient = (double) dividend / divisor;
+				System.out.printf("%.3f / %.3f = %.5f", dividend, divisor, quotient);
 				run = false;
 				scanner.close();
-		  /*}*/
-			}catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
 				scanner.next();
 				continue;
-			}catch(ArithmeticException e) {
-				System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요."/*e.getMessage()*/);
+			} catch (ArithmeticException e) {
+				System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요."/* e.getMessage() */);
 				continue;
 			}
 		}
