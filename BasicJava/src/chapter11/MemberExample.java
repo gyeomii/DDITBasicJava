0b731ae17dceb97e814e576ebf0a7d67a0ddc129
gyeomii <kimsg7388@gmail.com>
2022-05-12 11:48:50 +0900

    chapter11

---
+++ BasicJava/src/chapter11/MemberExample.java
@@ -0,0 +1,21 @@
+package chapter11;
+
+public class MemberExample {
+	public static void main(String[] args) {
+		Member obj1 = new Member("blue");
+		Member obj2 = new Member("blue");
+		Member obj3 = new Member("red");
+		
+		if(obj1.equals(obj2)) {
+			System.out.println("동등");
+		}else{
+			System.out.println("비동등");
+		}
+		if(obj1.equals(obj3)) {
+			System.out.println("동등");
+		}else{
+			System.out.println("비동등");
+		}
+		
+	}
+}
