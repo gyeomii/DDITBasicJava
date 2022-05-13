08c34c76fa6a388f35ba39a905d22d73fa5088c9
gyeomii <kimsg7388@gmail.com>
2022-05-13 12:50:18 +0900

    String Example

--- BasicJava/src/chapter11/StringExample/ByteToStringExample.java
+++ BasicJava/src/chapter11/StringExample/ByteToStringExample.java
@@ -1,4 +1,4 @@
-package chapter11;
+package chapter11.StringExample;
 
 public class ByteToStringExample {
 	public static void main(String[] args) {
---
+++ BasicJava/src/chapter11/StringExample/StringEqualsExample.java
@@ -0,0 +1,23 @@
+package chapter11.StringExample;
+
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVal1 = new String("신민철");
+		String strVal2 = "신민철";
+		
+		if (strVal1 == strVal2) {
+			System.out.println("같은 String객체를 참조");
+		} else {
+			System.out.println("다른 String객체를 참조");
+		}
+
+		if (strVal1.equals(strVal2)) {
+			System.out.println("같은 문자열을 가짐");
+		} else {
+			System.out.println("다른 문자열을 가짐");
+		}
+		
+		
+		
+	}
+}
